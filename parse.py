65a3194d8d28cf49b4f541f68ae4c23229b6a680
Sergio Ugalde Marcano <serch037@gmail.com>
2017-07-14 10:52:10 -0500

    Fixed linting errors

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 
+
 # Class definition
 class ITESMClass:
     def __init__(self, name, code, time_begin,
@@ -18,7 +19,8 @@ file2 = file1.read()
 soup = BeautifulSoup(file2, 'lxml')
 table = soup.find_all('tbody')[2]
 rows = table.find_all('tr')
-text = ['MA2009','MA1006','F1005','H2001','TC1020','TC2017','TC2018','TC2019']
+text = ['MA2009', 'MA1006', 'F1005', 'H2001', 'TC1020', 'TC2017', 'TC2018',
+        'TC2019']
 classes = []
 for row in rows:
     cols = row.find_all('td')
@@ -29,8 +31,8 @@ for row in rows:
         time_end = cols[10].text.strip()
         days = cols[11].text.strip()
         teacher = cols[14].text.strip()
-        classes.append(ITESMClass(name, code, time_begin, time_end, days,
-                                  teacher))
+        classes.append(ITESMClass(name, code, time_begin, time_end,
+                                  days, teacher))
 
 classes = sorted(classes, key=lambda course: course.time_end)
 for course in classes:
